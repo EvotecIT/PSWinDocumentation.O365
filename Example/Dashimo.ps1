bf9ee3cb8691379e3cc50056bb5a2aae1903848c
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-30 21:11:49 +0200

    Examples for Office 365

---
+++ Example/Dashimo.ps1
@@ -0,0 +1,54 @@
+﻿#Import-Module .\PSWinDocumentation.O365.psd1 -Force
+
+#Connect-WinConnectivity -UserName 'przemyslaw.klys@evotec.pl' -FilePath 'C:\Support\Important\Password-O365-Evotec.txt' -AsSecure -Service AzureAD, MSOnline, ExchangeOnline, Teams, SkypeOnline
+
+#if (-not $O365) {
+#$O365 = Get-WinO365 -Formatted -Verbose
+#}
+
+Dashboard -Name 'O365 Dashboard' -FilePath $PSScriptRoot\Output\Dashboard.html {
+    Tab -Name 'Teams' -IconBrands the-red-yeti {
+        Section -Invisible {
+            Section -Name 'Teams Settings' {
+                Table -DataTable $O365.TeamsSettings -Filtering {
+                    #TableHeader -Title 'Teams Settings' -Color Black -BackGroundColor Gray
+                }
+            }
+            Section -Name 'Teams File Sharing' {
+                Table -DataTable $O365.TeamsSettingsFileSharing -Filtering
+            }
+        }
+        Section -Invisible {
+            Section -Name 'Teams Settings for Channel' {
+                Table -DataTable $O365.TeamsSettingsChannels -Filtering
+            }
+            Section -Name 'Teams Settings for Guests' {
+                Table -DataTable $O365.TeamsSettingsGuests -Filtering
+            }
+        }
+        Section -Invisible {
+            Section -Name 'Teams Upgrade Options' {
+                Table -DataTable $O365.TeamsSettingsUpgrade -Filtering
+            }
+            Section -Name 'Teams Settings for Users' {
+                Table -DataTable $O365.TeamsSettingsUsers -Filtering
+            }
+        }
+        Section -Invisible {
+            Section -Name 'Teams Broadcasting' {
+                Table -DataTable $O365.TeamsSettingsBroadcasting -Filtering
+            }
+            Section -Name 'Teams Calling' {
+                Table -DataTable $O365.TeamsSettingsCalling -Filtering
+            }
+        }
+        Section -Invisible {
+            Section -Name 'Teams Meetings' {
+                Table -DataTable $O365.TeamsSettingsMeetings -Filtering
+            }
+            Section -Name 'Teams Meetings Technical' {
+                Table -DataTable $O365.TeamsSettingsMeetingsTechnical -Filtering
+            }
+        }
+    }
+} -Show
---
+++ Example/Documentimo.ps1
@@ -0,0 +1,45 @@
+﻿$CompanyName = 'Evotec'
+
+Documentimo -FilePath "$PSScriptRoot\Output\Office365-Documentation.docx" {
+    DocToc -Title 'Table of content'
+
+    DocPageBreak
+
+    DocText {
+        "This document provides documentation of Office 365 for $CompanyName."
+    }
+    DocNumbering -Text 'Microsoft Teams' -Level 0 -Type Numbered -Heading Heading1 {
+        DocText -Text "Following section provides overview of settings for Microsoft Teams and it's relation to Skype For Business"
+
+        DocNumbering -Text 'Global Settings' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettings -OverwriteTitle 'Global Settings'
+        }
+        DocNumbering -Text 'File Sharing Options' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsFileSharing -OverwriteTitle 'File Sharing Options'
+        }
+        DocNumbering -Text 'Channel Settings' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsChannels -OverwriteTitle 'Channel Settings'
+        }
+        DocNumbering -Text 'Settings for Guests' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsGuests -OverwriteTitle 'Settings for Guests'
+        }
+        DocNumbering -Text 'Settings for Upgrading Skype to Teams' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsUpgrade -OverwriteTitle 'Settings for Upgrading Skype to Teams'
+        }
+        DocNumbering -Text 'Settings for Users' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsUsers -OverwriteTitle 'Settings for Users'
+        }
+        DocNumbering -Text 'Broadcasting Settings' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsBroadcasting -OverwriteTitle 'Broadcasting Settings'
+        }
+        DocNumbering -Text 'Calling Settings' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsCalling -OverwriteTitle 'Calling Settings'
+        }
+        DocNumbering -Text 'Meetings Settings' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsMeetings -OverwriteTitle 'Meetings Settings'
+        }
+        DocNumbering -Text 'Meetings Technical Information' -Level 1 -Type Numbered -Heading Heading1 {
+            DocTable -DataTable $O365.TeamsSettingsMeetingsTechnical -OverwriteTitle 'Meetings Technical Information'
+        }
+    }
+} -Open
